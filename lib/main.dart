8f4d929b33aff61ee25d9d0da88d84218b6cd484
VibhaKurpad <40729467+VibhaKurpad@users.noreply.github.com>
2021-08-19 18:21:20 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class App extends StatelessWidget {
         buttonTheme: Theme.of(context).buttonTheme.copyWith(
               highlightColor: Colors.red,
             ),
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.red,
         textTheme: GoogleFonts.robotoTextTheme(
           Theme.of(context).textTheme,
         ),
@@ -74,7 +74,7 @@ class HomePage extends StatelessWidget {
           ),
           const Header("What we'll be doing"),
           const Paragraph(
-            'Join us for a day full of Firebase Workshops and Burger!',
+            'Join us for a day full of Firebase Workshops and Pizza and Fries!',
           ),
           Consumer<ApplicationState>(
             builder: (context, appState, _) => Column(
